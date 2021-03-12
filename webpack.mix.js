a46c031205d43393110e20be1e18bb06cdcacf66
Dominik Ježík <dominikjezik303@gmail.com>
2021-03-12 18:58:54 +0100

    Added Quill styling and created new scss file for vendor.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/scss/app.scss', 'public/css')
+    .sass('resources/scss/vendor.scss', 'public/css')
     .webpackConfig(require('./webpack.config'));
 if (mix.inProduction()) {
     mix.version();
