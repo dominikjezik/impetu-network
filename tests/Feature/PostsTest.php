8ac89b0d4a9a625e398f1e91b15301e9b69bae7b
Dominik Ježík <dominikjezik303@gmail.com>
2021-04-25 15:32:59 +0200

    Added tests for SubPage management.

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -3,28 +3,46 @@
 namespace Tests\Feature;
 
 use App\Models\Post;
+use App\Models\Role;
 use App\Models\SubPage;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Jetstream\Jetstream;
 use Tests\TestCase;
 
 class PostsTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->setInitialRoles();
+    }
+
+    private function setInitialRoles()
+    {
+        Role::create([
+            'type' => 'owner',
+            'title' => 'Owner'
+        ]);
+        Role::create([
+            'type' => 'admin',
+            'title' => 'Admin'
+        ]);
+        Role::create([
+            'type' => 'moderator',
+            'title' => 'Moderator'
+        ]);
+    }
+
     /** @test */
     public function a_title_of_post_is_required()
     {
         $user = User::factory()->create();
+        $this->actingAs($user);
         $subPage = SubPage::factory()->create();
 
-        $subPage->joinMember($user);
-
-        $this->actingAs($user);
-
-        $this->post("/r/$subPage->name/posts", [
+        $this->post(route('posts.store', $subPage), [
             'title' => "",
             'body' => "Lorem ipsum dolor sit amet."
         ]);
@@ -36,13 +54,10 @@ class PostsTest extends TestCase
     public function a_body_of_post_is_required()
     {
         $user = User::factory()->create();
+        $this->actingAs($user);
         $subPage = SubPage::factory()->create();
 
-        $subPage->joinMember($user);
-
-        $this->actingAs($user);
-
-        $this->post("/r/$subPage->name/posts", [
+        $this->post(route('posts.store', $subPage), [
             'title' => "Hello world!",
             'body' => ""
         ]);
@@ -53,12 +68,13 @@ class PostsTest extends TestCase
     /** @test */
     public function only_member_can_publish_a_post_in_sub_page()
     {
-        $user = User::factory()->create();
+        $this->actingAs(User::factory()->create());
         $subPage = SubPage::factory()->create();
 
+        $user = User::factory()->create();
         $this->actingAs($user);
 
-        $this->post("/r/$subPage->name/posts", [
+        $this->post(route('posts.store', $subPage), [
             'title' => "Hello world!",
             'body' => "Lorem ipsum dolor sit amet."
         ]);
@@ -67,7 +83,7 @@ class PostsTest extends TestCase
 
         $subPage->joinMember($user);
 
-        $response = $this->post("/r/$subPage->name/posts", [
+        $response = $this->post(route('posts.store', $subPage), [
             'title' => "Hello world!",
             'body' => "Lorem ipsum dolor sit amet."
         ]);
@@ -78,14 +94,46 @@ class PostsTest extends TestCase
     }
 
     /** @test */
+    public function an_author_of_the_post_can_update_that_post()
+    {
+        $this->actingAs(User::factory()->create());
+        $subPage = SubPage::factory()->create();
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $subPage->joinMember($user);
+
+        $this->post(route('posts.store', $subPage), [
+            'title' => "Hello world!",
+            'body' => "Lorem ipsum dolor sit amet."
+        ]);
+
+        $post = Post::first();
+
+        $updatedTitle = "Updated title";
+        $updatedBody = "Updated body";
+
+        $this->patch(route('posts.update', [$subPage, $post]), [
+            'title' => $updatedTitle,
+            'body' => $updatedBody
+        ]);
+
+        $post->refresh();
+
+        $this->assertEquals($updatedTitle, $post->title);
+        $this->assertEquals($updatedBody, $post->body);
+    }
+
+    /** @test */
     public function an_author_of_post_can_delete_his_post()
     {
         $this->withExceptionHandling();
+        $this->actingAs(User::factory()->create());
         $post = Post::factory()->create();
 
         $this->actingAs($post->author);
 
-        $this->delete("/r/{$post->subPage->name}/$post->id");
+        $this->delete(route('posts.destroy', [$post->subPage, $post]));
 
         $this->assertEquals(0, Post::count());
     }
@@ -93,12 +141,51 @@ class PostsTest extends TestCase
     /** @test */
     public function an_user_cannot_delete_foreign_post()
     {
+        $this->actingAs(User::factory()->create());
+
         $subPage = SubPage::factory()->create();
         $post = Post::factory()->create();
 
         $this->actingAs(User::factory()->create());
 
-        $this->delete("/r/$subPage->name/$post->id");
+        $this->delete(route('posts.destroy', [$subPage, $post]));
+
+        $this->assertEquals(1, Post::count());
+    }
+
+    /** @test */
+    public function an_moderator_of_sub_page_can_delete_member_post()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(User::factory()->create());
+        $subPage = SubPage::factory()->create();
+        $post = Post::factory()->create(['sub_page_id' => $subPage->id]);
+
+        $moderator = User::factory()->create();
+        $subPage->joinMember($moderator);
+        $subPage->tryToSetRole('moderator', $moderator);
+        $this->actingAs($moderator);
+
+        $this->delete(route('posts.destroy', [$subPage, $post]));
+
+        $this->assertEquals(0, Post::count());
+    }
+
+    /** @test */
+    public function an_moderator_of_sub_page_one_can_not_delete_posts_of_sub_page_two()
+    {
+        $this->actingAs(User::factory()->create());
+        $subPageOne = SubPage::factory()->create();
+        $post = Post::factory()->create(['sub_page_id' => $subPageOne->id]);
+
+        $subPageTwo = SubPage::factory()->create();
+        $moderator = User::factory()->create();
+        $subPageOne->joinMember($moderator);
+        $subPageTwo->joinMember($moderator);
+        $subPageTwo->tryToSetRole('moderator', $moderator);
+        $this->actingAs($moderator);
+
+        $this->delete(route('posts.destroy', [$subPageOne, $post]));
 
         $this->assertEquals(1, Post::count());
     }
--- tests/Feature/SubPagesTest.php
+++ tests/Feature/SubPagesTest.php
@@ -2,23 +2,46 @@
 
 namespace Tests\Feature;
 
+use App\Models\Role;
 use App\Models\SubPage;
 use App\Models\User;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Jetstream\Jetstream;
 use Tests\TestCase;
 
 class SubPagesTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->setInitialRoles();
+    }
+
+    private function setInitialRoles()
+    {
+        Role::create([
+            'type' => 'owner',
+            'title' => 'Owner'
+        ]);
+        Role::create([
+            'type' => 'admin',
+            'title' => 'Admin'
+        ]);
+        Role::create([
+            'type' => 'moderator',
+            'title' => 'Moderator'
+        ]);
+    }
+
     /** @test */
     public function a_sub_page_screen_can_be_rendered()
     {
         $this->withoutExceptionHandling();
+        $this->actingAs(User::factory()->create());
+
         $subPage = SubPage::factory()->create();
-        $response = $this->get("/r/$subPage->name");
+        $response = $this->get(route('subpages.show', $subPage));
 
         $response->assertStatus(200);
     }
@@ -28,16 +51,19 @@ class SubPagesTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $user = User::factory()->create();
-        $this->actingAs($user);
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
 
         $subPage = SubPage::factory()->create();
 
-        $response = $this->post("/r/$subPage->name/join");
+        $user = User::factory()->create();
+        $this->actingAs($user);
 
-        $response->assertRedirect("/r/$subPage->name");
+        $response = $this->post(route('subpages.join', $subPage));
 
-        $this->assertEquals(1, $subPage->members->count());
+        $response->assertRedirect(route('subpages.show', $subPage));
+
+        $this->assertEquals(2, $subPage->members->count());
         $this->assertTrue($subPage->members->contains($user));
     }
 
@@ -46,31 +72,37 @@ class SubPagesTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
         $user = User::factory()->create();
         $this->actingAs($user);
-
-        $subPage = SubPage::factory()->create();
         $subPage->joinMember($user);
 
-        $this->assertEquals(1, $subPage->members->count());
+        $this->assertEquals(2, $subPage->members->count());
         $this->assertTrue($subPage->members->contains($user));
 
-        $response = $this->post("/r/$subPage->name/leave");
-        $response->assertRedirect("/r/$subPage->name");
+        $response = $this->post(route('subpages.leave', $subPage));
+        $response->assertRedirect(route('subpages.show', $subPage));
 
         $subPage->load('members');
 
-        $this->assertEquals(0, $subPage->members->count());
+        $this->assertEquals(1, $subPage->members->count());
         $this->assertFalse($subPage->members->contains($user));
     }
 
     /** @test */
     public function only_auth_user_can_join_sub_page()
     {
+        $this->actingAs(User::factory()->create());
         $subPage = SubPage::factory()->create();
-        $response = $this->post("/r/$subPage->name/join");
 
-        $this->assertCount(0, $subPage->members);
+        auth()->logout();
+
+        $response = $this->post(route('subpages.join', $subPage));
+
+        $this->assertCount(1, $subPage->members);
 
         $response->assertRedirect();
     }
@@ -78,27 +110,250 @@ class SubPagesTest extends TestCase
     /** @test */
     public function only_auth_user_can_leave_sub_page()
     {
+        $this->actingAs(User::factory()->create());
         $subPage = SubPage::factory()->create();
-        $response = $this->post("/r/$subPage->name/leave");
 
-        $this->assertCount(0, $subPage->members);
+        auth()->logout();
+
+        $response = $this->post(route('subpages.leave', $subPage));
+
+        $this->assertCount(1, $subPage->members);
 
         $response->assertRedirect();
     }
 
+    /** @test */
+    public function auth_user_can_create_sub_page()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
 
-//
-//    public function test_new_users_can_register()
-//    {
-//        $response = $this->post('/register', [
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//            'password' => 'password',
-//            'password_confirmation' => 'password',
-//            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
-//        ]);
-//
-//        $this->assertAuthenticated();
-//        $response->assertRedirect(RouteServiceProvider::HOME);
-//    }
+        $this->post(route('subpages.store'), [
+            'name' => 'test',
+            'title' => 'Test community',
+            'description' => 'Lorem ipsum dolor sit amet.'
+        ]);
+
+        $this->assertEquals(1, SubPage::count());
+    }
+
+    /** @test */
+    public function guest_can_not_create_sub_page()
+    {
+        $this->post(route('subpages.store'), [
+            'name' => 'test',
+            'title' => 'Test community',
+            'description' => 'Lorem ipsum dolor sit amet.'
+        ]);
+
+        $this->assertEquals(0, SubPage::count());
+    }
+
+    /** @test */
+    public function name_is_required_to_create_a_new_sub_page()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $this->post(route('subpages.store'), [
+            'name' => '',
+            'title' => 'Test community',
+            'description' => 'Lorem ipsum dolor sit amet.'
+        ]);
+
+        $this->assertEquals(0, SubPage::count());
+    }
+
+    /** @test */
+    public function title_is_required_to_create_a_new_sub_page()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $this->post(route('subpages.store'), [
+            'name' => 'test',
+            'title' => '',
+            'description' => 'Lorem ipsum dolor sit amet.'
+        ]);
+
+        $this->assertEquals(0, SubPage::count());
+    }
+
+    /** @test */
+    public function description_is_required_to_create_a_new_sub_page()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $this->post(route('subpages.store'), [
+            'name' => 'test',
+            'title' => 'Test community',
+            'description' => ''
+        ]);
+
+        $this->assertEquals(0, SubPage::count());
+    }
+
+    /** @test */
+    public function users_with_a_role_can_view_the_manage_page()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(User::factory()->create());
+
+        $subPage = SubPage::factory()->create();
+        $response = $this->get(route('subpages.edit', $subPage));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function users_without_a_role_can_not_view_the_manage_page()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->get(route('subpages.edit', $subPage));
+        $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function a_basic_information_can_be_updated_by_owner()
+    {
+        $this->withoutExceptionHandling();
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $this->patch(route('subpages.udpate', $subPage), [
+            'title' => 'Updated title',
+            'description' => 'Updated description'
+        ]);
+
+        $subPage->refresh();
+
+        $this->assertEquals('Updated title', $subPage->title);
+        $this->assertEquals('Updated description', $subPage->description);
+    }
+
+
+    /** @test */
+    public function a_basic_information_can_be_updated_by_admin()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $admin = User::factory()->create();
+        $subPage->joinMember($admin);
+        $subPage->tryToSetRole('admin', $admin);
+        $this->actingAs($admin);
+
+        $this->patch(route('subpages.udpate', $subPage), [
+            'title' => 'Updated title',
+            'description' => 'Updated description'
+        ]);
+
+        $subPage->refresh();
+
+        $this->assertEquals('Updated title', $subPage->title);
+        $this->assertEquals('Updated description', $subPage->description);
+    }
+
+    /** @test */
+    public function a_basic_information_can_not_be_updated_by_moderator()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $moderator = User::factory()->create();
+        $subPage->joinMember($moderator);
+        $subPage->tryToSetRole('moderator', $moderator);
+        $this->actingAs($moderator);
+
+        $originalTitle = $subPage->title;
+        $originalDescription = $subPage->description;
+
+        $this->patch(route('subpages.udpate', $subPage), [
+            'title' => 'Updated title',
+            'description' => 'Updated description'
+        ]);
+
+        $subPage->refresh();
+
+        $this->assertEquals($originalTitle, $subPage->title);
+        $this->assertEquals($originalDescription, $subPage->description);
+    }
+
+    /** @test */
+    public function a_basic_member_can_not_update_basic_information()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $user = User::factory()->create();
+        $subPage->joinMember($user);
+        $this->actingAs($user);
+
+        $originalTitle = $subPage->title;
+        $originalDescription = $subPage->description;
+
+        $this->patch(route('subpages.udpate', $subPage), [
+            'title' => 'Updated title',
+            'description' => 'Updated description'
+        ]);
+
+        $this->assertEquals($originalTitle, $subPage->title);
+        $this->assertEquals($originalDescription, $subPage->description);
+    }
+
+    /** @test */
+    public function an_owner_of_sub_page_can_delete_sub_page()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $this->delete(route('subpages.destroy', $subPage));
+
+        $this->assertEmpty(SubPage::all());
+    }
+
+    /** @test */
+    public function other_roles_of_sub_page_can_not_delete_sub_page()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $admin = User::factory()->create();
+        $subPage->joinMember($admin);
+        $subPage->tryToSetRole('admin', $admin);
+        $this->actingAs($admin);
+
+        $this->delete(route('subpages.destroy', $subPage));
+
+        $this->assertCount(1, SubPage::all());
+    }
+
+    /** @test */
+    public function a_basic_member_can_not_delete_sub_page()
+    {
+        $owner = User::factory()->create();
+        $this->actingAs($owner);
+        $subPage = SubPage::factory()->create();
+
+        $user = User::factory()->create();
+        $subPage->joinMember($user);
+        $this->actingAs($user);
+
+        $this->delete(route('subpages.destroy', $subPage));
+
+        $this->assertCount(1, SubPage::all());
+    }
+
 }
