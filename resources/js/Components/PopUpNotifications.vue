65409bf0b9b822012ceb204728febb077a4b8161
Dominik Ježík <dominikjezik303@gmail.com>
2021-04-30 19:06:18 +0200

    Fixed pop-up notification time.

--- resources/js/Components/PopUpNotifications.vue
+++ resources/js/Components/PopUpNotifications.vue
@@ -35,7 +35,7 @@ export default {
                 if (index !== -1) {
                     this.notifications.splice(index, 1);
                 }
-            }, 100000)
+            }, 4000)
 
         }
     },
